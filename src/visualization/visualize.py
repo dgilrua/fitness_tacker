f091316dd1f2a5ad0c5e55517739b8bf80e6e84d
David Gil Rua <dgilr@unal.edu.co>
2024-08-26 13:10:23 -0500

    data vis 2/2 end

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -47,8 +47,6 @@ participant_df = (
     df.query("label == 'bench'").sort_values("participant").reset_index(drop=True)
 )
 
-import seaborn as sns
-
 fig, ax = plt.subplots()
 participant_df.groupby("participant")["acc_y"].plot(legend=True)
 plt.show()
@@ -58,16 +56,78 @@ plt.show()
 # Plot multiple axis
 # --------------------------------------------------------------
 
+label = "squat"
+participant = "A"
+
+all_axis_df = df.query(
+    f"label == '{label}' & participant == '{participant}'"
+).reset_index(drop=True)
+fig, ax = plt.subplots()
+all_axis_df[["acc_x", "acc_y", "acc_z"]].plot(ax=ax, legend=True)
+
 
 # --------------------------------------------------------------
 # Create a loop to plot all combinations per sensor
 # --------------------------------------------------------------
 
+labels = df.label.unique()
+participants = df.participant.unique()
+
+for label in labels:
+    for participant in participants:
+        all_axis_df = df.query(
+            f"label == '{label}' & participant == '{participant}'"
+        ).reset_index(drop=True)
+
+        if len(all_axis_df) > 0:
+            fig, ax = plt.subplots()
+            all_axis_df[["acc_x", "acc_y", "acc_z"]].plot(ax=ax, legend=True)
+            plt.show()
+
+for label in labels:
+    for participant in participants:
+        all_axis_df = df.query(
+            f"label == '{label}' & participant == '{participant}'"
+        ).reset_index(drop=True)
+
+        if len(all_axis_df) > 0:
+            fig, ax = plt.subplots()
+            all_axis_df[["gyr_x", "gyr_y", "gyr_z"]].plot(ax=ax, legend=True)
+            plt.show()
 
 # --------------------------------------------------------------
 # Combine plots in one figure
 # --------------------------------------------------------------
 
+for label in labels:
+    for participant in participants:
+
+        all_axis_df = df.query(
+            f"label == '{label}' & participant == '{participant}'"
+        ).reset_index(drop=True)
+
+        if len(all_axis_df) > 0:
+            fig, ax = plt.subplots(2, sharex=True, figsize=(20, 14))
+            all_axis_df[["acc_x", "acc_y", "acc_z"]].plot(ax=ax[0], legend=True)
+            fig.suptitle(f"{label} - {participant}".title(), fontsize=16)
+            all_axis_df[["gyr_x", "gyr_y", "gyr_z"]].plot(ax=ax[1], legend=True)
+            ax[0].legend(
+                loc="upper center",
+                bbox_to_anchor=(0.5, 1.15),
+                ncol=3,
+                fancybox=True,
+                shadow=True,
+            )
+            ax[1].legend(
+                loc="upper center",
+                bbox_to_anchor=(0.5, 1.15),
+                ncol=3,
+                fancybox=True,
+                shadow=True,
+            )
+            ax[1].set_xlabel("Samples")
+            plt.savefig(f"../../reports/figures/{label.title()} ({participant}).png")
+            plt.show()
 
 # --------------------------------------------------------------
 # Loop over all combinations and export for both sensors
