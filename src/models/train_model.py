54a6834bd1a1541ccf6c003b673e2097eee794b9
David Gil Rua <dgilr@unal.edu.co>
2024-08-28 13:06:52 -0500

    feature selection and train model

---
+++ src/models/train_model.py
@@ -0,0 +1,219 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+from LearningAlgorithms import ClassificationAlgorithms
+import seaborn as sns
+import itertools
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+
+# Plot settings
+plt.style.use("fivethirtyeight")
+plt.rcParams["figure.figsize"] = (20, 5)
+plt.rcParams["figure.dpi"] = 100
+plt.rcParams["lines.linewidth"] = 2
+
+df = pd.read_pickle("../../data/interim/data_clustered.pkl")
+
+# --------------------------------------------------------------
+# Create a training and test set
+# --------------------------------------------------------------
+
+df_train = df.drop(["participant", "category", "set", "duration"], axis=1)
+
+X = df_train.drop("label", axis=1)
+y = df_train["label"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.25, random_state=42, stratify=y
+)
+
+fig, ax = plt.subplots(figsize=(10, 5))
+df_train["label"].value_counts().plot(kind="bar", ax=ax, color="skyblue", label="Total")
+y_train.value_counts().plot(kind="bar", ax=ax, color="salmon", label="Train")
+y_test.value_counts().plot(kind="bar", ax=ax, color="lightgreen", label="Test")
+plt.legend()
+plt.show()
+
+# --------------------------------------------------------------
+# Split feature subsets
+# --------------------------------------------------------------
+
+basic_features = ["acc_x", "acc_y", "acc_z", "gyr_x", "gyr_y", "gyr_z"]
+pca_features = ["pca_1", "pca_2", "pca_3"]
+square_features = ["acc_r", "gyr_r"]
+time_features = [i for i in df_train.columns if "_temp_" in i]
+freq_features = [i for i in df_train.columns if ("_freq" in i) or ("_pse" in i)]
+cluster_features = ["cluster"]
+
+features_1 = basic_features
+features_2 = list(set(basic_features + pca_features + square_features))
+features_3 = list(set(features_2 + time_features))
+features_4 = list(set(features_3 + freq_features + cluster_features))
+
+selected_features = [
+    "acc_z_freq_0.0_Hz_ws_14",
+    "acc_x_freq_0.0_Hz_ws_14",
+    "gyr_r_freq_0.0_Hz_ws_14",
+    "acc_z",
+    "acc_y_freq_0.0_Hz_ws_14",
+    "gyr_r_freq_1.429_Hz_ws_14",
+    "gyr_z_max_freq",
+    "acc_z_freq_2.143_Hz_ws_14",
+    "acc_z_freq_2.5_Hz_ws_14",
+    "gyr_y_pse",
+]
+
+# --------------------------------------------------------------
+# Perform forward feature selection using simple decision tree
+# --------------------------------------------------------------
+
+learner = ClassificationAlgorithms()
+max_features = 10
+"""selected_features, ordered_features, ordered_scores = learner.forward_selection(
+    max_features, X_train, y_train
+)"""
+
+fig, ax = plt.subplots(figsize=(10, 7))
+plt.plot(range(1, max_features + 1), ordered_scores, marker="o")
+plt.xticks(range(1, max_features + 1))
+plt.xlabel("Number of features")
+plt.ylabel("Accuracy")
+plt.title("Forward feature selection")
+plt.show()
+
+# --------------------------------------------------------------
+# Grid search for best hyperparameters and model selection
+# --------------------------------------------------------------
+
+possible_feature_sets = [
+    features_1,
+    features_2,
+    features_3,
+    features_4,
+    selected_features,
+]
+
+feature_names = [
+    "Features set 1",
+    "Features set 2",
+    "Features set 3",
+    "Features set 4",
+    "Selected features",
+]
+
+iterations = 1
+
+for i, f in zip(range(len(possible_feature_sets)), feature_names):
+    print("Feature set:", i)
+    selected_train_X = X_train[possible_feature_sets[i]]
+    selected_test_X = X_test[possible_feature_sets[i]]
+
+    # First run non deterministic classifiers to average their score.
+    performance_test_nn = 0
+    performance_test_rf = 0
+
+    for it in range(0, iterations):
+        print("\tTraining neural network,", it)
+        (
+            class_train_y,
+            class_test_y,
+            class_train_prob_y,
+            class_test_prob_y,
+        ) = learner.feedforward_neural_network(
+            selected_train_X,
+            y_train,
+            selected_test_X,
+            gridsearch=False,
+        )
+        performance_test_nn += accuracy_score(y_test, class_test_y)
+
+        print("\tTraining random forest,", it)
+        (
+            class_train_y,
+            class_test_y,
+            class_train_prob_y,
+            class_test_prob_y,
+        ) = learner.random_forest(
+            selected_train_X, y_train, selected_test_X, gridsearch=True
+        )
+        performance_test_rf += accuracy_score(y_test, class_test_y)
+
+    performance_test_nn = performance_test_nn / iterations
+    performance_test_rf = performance_test_rf / iterations
+
+    # And we run our deterministic classifiers:
+    print("\tTraining KNN")
+    (
+        class_train_y,
+        class_test_y,
+        class_train_prob_y,
+        class_test_prob_y,
+    ) = learner.k_nearest_neighbor(
+        selected_train_X, y_train, selected_test_X, gridsearch=True
+    )
+    performance_test_knn = accuracy_score(y_test, class_test_y)
+
+    print("\tTraining decision tree")
+    (
+        class_train_y,
+        class_test_y,
+        class_train_prob_y,
+        class_test_prob_y,
+    ) = learner.decision_tree(
+        selected_train_X, y_train, selected_test_X, gridsearch=True
+    )
+    performance_test_dt = accuracy_score(y_test, class_test_y)
+
+    print("\tTraining naive bayes")
+    (
+        class_train_y,
+        class_test_y,
+        class_train_prob_y,
+        class_test_prob_y,
+    ) = learner.naive_bayes(selected_train_X, y_train, selected_test_X)
+
+    performance_test_nb = accuracy_score(y_test, class_test_y)
+
+    # Save results to dataframe
+    models = ["NN", "RF", "KNN", "DT", "NB"]
+    new_scores = pd.DataFrame(
+        {
+            "model": models,
+            "feature_set": f,
+            "accuracy": [
+                performance_test_nn,
+                performance_test_rf,
+                performance_test_knn,
+                performance_test_dt,
+                performance_test_nb,
+            ],
+        }
+    )
+    score_df = pd.concat([score_df, new_scores])
+
+# --------------------------------------------------------------
+# Create a grouped bar plot to compare the results
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Select best model and evaluate results
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Select train and test data based on participant
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Use best model again and evaluate results
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Try a simpler model with the selected features
+# --------------------------------------------------------------
